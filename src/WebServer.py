a9ecae109ff12f2b2a08a31ccd8b1542a48d54df
Alex Ward <alxwrd@googlemail.com>
2017-09-27 21:36:50 +0100

    routing on webserver

--- src/WebServer.py
+++ src/WebServer.py
@@ -11,12 +11,20 @@ class WebServer(bottle.Bottle):
         super().__init__()
         self.almanac = Almanac()
 
-        self.route("/", method="GET", callback=self.index)
         self.route("/upload", method="POST", callback=self.upload)
 
+        self.route("/", method="GET", callback=self.index)
+        self.route("/almanac/<ocean>", method="GET", callback=self.oceans)
+        self.route("/almanac/<ocean>/<island>", method="GET", callback=self.islands)
+        self.route("/almanac/<ocean>/<island>/<commodity>", method="GET", callback=self.commodities)
 
-    def index(self):
-        return bottle.template("site\\index.html")
+        #Static files
+        self.route("/css/<name>", method='GET',
+                   callback=lambda name: bottle.static_file(name, root="./site/css"))
+        self.route("/js/<name>", method='GET',
+                   callback=lambda name: bottle.static_file(name, root="./site/js"))
+        self.route("/img/<name>", method='GET',
+                   callback=lambda name: bottle.static_file(name, root="./site/img"))
 
 
     def upload(self):
@@ -26,6 +34,26 @@ class WebServer(bottle.Bottle):
         return ""
 
 
+    def index(self):
+        return bottle.template("site/index.html", almanac=self.almanac)
+
+
+    def oceans(self, ocean):
+        return bottle.template(
+            "site/ocean.html",
+            ocean=self.almanac.oceans[ocean])
+
+
+    def islands(self, ocean, island):
+        return bottle.template(
+            "site/island.html",
+            island=self.almanac.oceans[ocean].islands[island])
+
+
+    def commodities(self, ocean, island, commodity):
+        return bottle.template(
+            "site/commodity.html",
+            commodity=self.almanac.oceans[ocean].islands[island].commodities[commodity])
 
 if __name__ == "__main__":
     app = WebServer()
